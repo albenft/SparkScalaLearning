dfea7ec0f35a9d45e251e3c620ebfb05bddb8a64
alben <albenft@gmail.com>
2020-06-08 21:41:48 +0700

    completed UDFDemo project

--- UDFDemo/src/main/scala/data/alben/spark/examples/UDFDemo.scala
+++ UDFDemo/src/main/scala/data/alben/spark/examples/UDFDemo.scala
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 
 object UDFDemo extends Serializable {
-  
+
   @transient lazy val logger : Logger = Logger.getLogger(getClass.getName)
 
   def main(args: Array[String]): Unit = {
@@ -22,8 +22,38 @@ object UDFDemo extends Serializable {
 
     // TODO
 
+    val surveyDF = spark.read
+        .format("csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load(args(0))
+
+
+    import org.apache.spark.sql.functions._
+    val parseGenderUDF = udf(parseGender(_: String): String)
+
+    spark.catalog.listFunctions().filter(r => r.name == "parseGenderUDF").show()
+
+    val transformedSurveyDF = surveyDF.withColumn("Gender", parseGenderUDF(col("Gender")))
+    transformedSurveyDF.show(10, false)
+
+    spark.udf.register("parseGenderUDF", parseGender(_: String): String)
+    spark.catalog.listFunctions().filter(r => r.name == "parseGenderUDF").show()
+
+    val transformedSurveyDF2 = surveyDF.withColumn("Gender", expr("parseGenderUDF(Gender)"))
+    transformedSurveyDF2.show(10, false)
+
     logger.info("Finished UDFDemo")
     spark.stop()
+  }
+
+  def parseGender(gender: String): String = {
+    val femalePattern = "^f$|f.m|w.m".r
+    val malePattern = "^m$|ma|m.l".r
+
+    if(femalePattern.findFirstIn(gender.toLowerCase).nonEmpty) "Female"
+    else if(malePattern.findFirstIn(gender.toLowerCase).nonEmpty) "Male"
+    else "Unknown"
   }
 
 }
