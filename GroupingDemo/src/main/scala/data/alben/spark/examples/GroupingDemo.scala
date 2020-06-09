f75a5e9ea49db6aa71470b501dd557515d0b14f4
alben <albenft@gmail.com>
2020-06-09 20:17:43 +0700

    completed quiz in my code and following course code

--- GroupingDemo/src/main/scala/data/alben/spark/examples/GroupingDemo.scala
+++ GroupingDemo/src/main/scala/data/alben/spark/examples/GroupingDemo.scala
@@ -2,6 +2,7 @@ package data.alben.spark.examples
 
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 
 object GroupingDemo extends Serializable {
 
@@ -21,7 +22,38 @@ object GroupingDemo extends Serializable {
         .option("inferSchema", "true")
         .load("data/invoices.csv")
 
-    invoiceDf.show(5)
+    /* My code
+    val transformedDf = invoiceDf.withColumn("InvoiceDate", to_date(col("InvoiceDate"), "dd-MM-yyyy H.m"))
+      .withColumn("WeekNumber", weekofyear(col("InvoiceDate")))
+
+    val aggDf = transformedDf.groupBy("Country","WeekNumber")
+        .agg(
+          countDistinct("InvoiceNo").as("NumInvoices"),
+          sum("Quantity").as("TotalQty"),
+          round(sum(expr("Quantity * UnitPrice")), 2).as("InvoiceValue")
+        ).orderBy(col("Country"), col("WeekNumber"))
+
+    aggDf.show(10, false)
+     */
+
+    // Course code
+
+    val numInvoices = countDistinct("InvoiceNo").as("NumInvoices")
+    val totalQty = sum("Quantity").as("TotalQty")
+    val invoiceValue = round(sum(expr("Quantity * UnitPrice")), 2).as("InvoiceValue")
+
+    val summaryDf = invoiceDf.withColumn("InvoiceDate", to_date(col("InvoiceDate"), "dd-MM-yyyy H.m"))
+      .where("year(InvoiceDate) == 2010")
+      .withColumn("WeekNumber", weekofyear(col("InvoiceDate")))
+      .groupBy(col("Country"), col("WeekNumber"))
+      .agg(numInvoices, totalQty, invoiceValue)
+
+    summaryDf.write
+      .format("parquet")
+      .mode("overwrite")
+      .save("dataSink/output")
+
+    summaryDf.sort("Country", "WeekNumber").show(10)
 
     logger.info("Finished GroupingDemo")
     spark.stop()
